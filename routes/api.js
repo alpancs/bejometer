7c325b6a5095efd87544118f9c6febab0dd08437
Alfan Nur Fauzan <alpancs@gmail.com>
2017-02-27 10:46:39 +0700

    simplify response and add bulk tebakgender

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,19 @@
 var router = require('express').Router()
 var tebakgender = require('tebakgender')
 
-router.get('/bejo/:name1/:name2', (req, res, next) => {
-  res.json({bejo: 0})
+router.get('/bejometer/:name1@:date1::name2@:date2', (req, res, next) => {
+  res.json(req.params)
 })
 
 router.get('/tebakgender/:name', (req, res, next) => {
-  res.json(tebakgender(req.params.name))
+  let prediction = tebakgender(req.params.name)
+  res.json({gender: prediction.gender, confidence: prediction.confidence})
+})
+
+router.get('/tebakgenders/:names', (req, res, next) => {
+  let names = req.params.names.split(/ *, */)
+  let predictions = names.map(tebakgender).map(prediction => ({gender: prediction.gender, confidence: prediction.confidence}))
+  res.json(predictions)
 })
 
 module.exports = router
