77029272779e3a8510048783365e8a778781d5eb
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-07 09:30:05 +0700

    add user agent into log

--- middleware/logger.js
+++ middleware/logger.js
@@ -7,6 +7,7 @@ module.exports = (req, res, next) => {
   logger.info({
     url: req.url,
     ip: req.ip,
+    user_agent: req.get('User-Agent'),
     elapsed_time: Date.now() - startTime,
   })
 }
