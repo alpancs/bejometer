698044664cfe40149f11327e59658c89163f2307
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-07 08:59:15 +0700

    fast and fair consultation algorithm

--- routes/api.js
+++ routes/api.js
@@ -60,25 +60,36 @@ let filterStudent = (student) => ({
 })
 
 let findSuggestions = (name, time, limit) => {
+  let targets = tebakgender(name).gender === 'L' ? people.P : people.L
+  let length = targets.length
   let minMatch = 1
+  let failure = 0
   let suggestions = []
-  let i = randomNumber()
+  let i = randomNumber(length)
   while (suggestions.length < limit) {
-    i = (i + randomNumber()) % students.length
-    let student = students[i]
-    let result = bejometer(name, time, student.name, student.date_of_birth)
+    i = (i + randomNumber(length)) % length
+    let target = targets[i]
+    let result = bejometer(name, time, target.name, target.date_of_birth)
     if (result.match >= minMatch) {
-      suggestions.push({match: result.match, person: filterStudent(student)})
-      minMatch *= 0.99
+      suggestions.push({match: result.match, person: filterStudent(target)})
+      failure >>= 1
+    } else {
+      ++failure
     }
+    if (failure > 100) minMatch *= 0.99
     if (suggestions.length === limit) break
   }
   return suggestions
 }
 
-let randomNumber = () => Math.floor(Math.random() * students.length)
+let randomNumber = (length) => ~~(Math.random() * length)
 
 for (let student of students)
   student.date_of_birth = Date.parse(student.date_of_birth)
+
+let people = {
+  L: students.filter((student) => student.gender === 'L'),
+  P: students.filter((student) => student.gender === 'P'),
+}
 
 module.exports = router
