0f2865c5c1345465e6c12c54f6ab7f82886fcb76
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-08 16:49:30 +0700

    scratching bejometer view

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,15 +1,22 @@
-var app = new Vue({
+new Vue({
   el: '#bejometer',
   data: {
-    name1: '',
-    date1: '',
-    name2: '',
-    date2: '',
     baseURL: location.origin,
+    name1: 'Alfan',
+    date1: '1994-07-28',
+    name2: 'Ayu',
+    date2: '1996-12-08',
+    bejometerResult: undefined,
   },
   methods: {
-    buildURL: function() {
-      return ''
-    }
-  }
+    updateBejometer: function() {
+      let name1 = this.name1
+      let date1 = this.date1
+      let name2 = this.name2
+      let date2 = this.date2
+      axios.get(`/api/bejometer/${name1}:${date1}&${name2}:${date2}`)
+        .then((response) => this.bejometerResult = response.data)
+        .catch((_error) => this.bejometerResult = null)
+    },
+  },
 })
