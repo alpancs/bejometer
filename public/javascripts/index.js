c20e15c42ed7423e84d157eb3dc49f30aa85ef13
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-12 01:04:58 +0700

    uggly view with css

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+  // $('ul.tabs').tabs()
+  // $('ul.tabs').tabs('select_tab', 'tebakgender')
+  // $(".button-collapse").sideNav()
+  // $('.carousel.carousel-slider').carousel({fullWidth: true})
+  $('.datepicker').pickadate({
+    selectMonths: true,
+    selectYears: 100,
+  })
+})
+
 let sanitize = function(name) {
   return name.replace(/[^A-Za-z']/g, ' ')
     .replace(/ +/g, ' ')
