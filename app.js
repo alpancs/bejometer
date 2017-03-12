d75500bd797f2b9a76823c291181590763cb094a
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-12 10:43:34 +0700

    prioritize compression

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(compression())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(compression())
 
 app.use(logger)
 app.use('/', index)
