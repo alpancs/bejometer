3a505050308a1c9cb5f0ec08ddfcec62e74c4db5
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-14 10:11:33 +0700

    change icon and text

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(require('middleware/logger'))
-
 app.use(compression())
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(require('middleware/logger'))
 app.use('/', require('routes/index'))
 app.use('/api', require('routes/api'))
 
