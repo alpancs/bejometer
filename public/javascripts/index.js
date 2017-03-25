6e328eb0c0bce40949f8412545c3ff01e88eda36
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-25 16:33:45 +0700

    refactor

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,60 +8,52 @@ Vue.material.registerTheme('default', {
 })
 
 new Vue({
-  el: '#index',
+  el: '#app',
 
   data: {
-    input: localStorage.input ? JSON.parse(localStorage.input) : {
-      bejometer: {},
-      consultation: {},
-      tebakgender: {},
-    },
-    requesting: {},
-    result: {},
-    error: {},
-    shareURL: {},
+    input:
+      localStorage.input ?
+      JSON.parse(localStorage.input) :
+      {bejometer: {}, consultation: {}, tebakgender: {}},
+
+    requestingBejometer: false,
+    resultBejometer: false,
+    errorBejometer: false,
+    shareURLBejometer: false,
   },
 
   methods: {
-    submit(app) {
-      this.requesting[app] = true
-      this.openDialog(`${app}Result`)
+    submitBejometer() {
+      this.requestingBejometer = true
+      this.openDialog('bejometerResult')
 
-      this.result[app] = null
-      this.shareURL[app] = ''
-      this.error[app] = null
+      this.resultBejometer = null
+      this.shareURLBejometer = null
+      this.errorBejometer = null
 
-      let params = this.buildParam(app)
-      axios.get(`/api/${app}`, {params})
+      let params = {
+        name1: this.input.bejometer.name1,
+        date1: this.input.bejometer.date1,
+        name2: this.input.bejometer.name2,
+        date2: this.input.bejometer.date2,
+      }
+      axios.get('/api/bejometer', {params})
         .then((response) => {
-          this.result[app] = response.data
-          this.shareURL[app] = this.buildShareURL(app)
-          console.log(this.result[app]);
+          this.resultBejometer = response.data
+          this.shareURLBejometer = this.buildShareURLBejometer()
         })
-        .catch((error) => this.error[app] = error)
-        .then(() => this.requesting[app] = false)
+        .catch((error) => this.errorBejometer = error)
+        .then(() => this.requestingBejometer = false)
 
       localStorage.input = JSON.stringify(this.input)
     },
 
-    buildParam(app) {
-      if (app === 'bejometer')
-        return {
-          name1: this.input.bejometer.name1,
-          date1: this.input.bejometer.date1,
-          name2: this.input.bejometer.name2,
-          date2: this.input.bejometer.date2,
-        }
-    },
-
-    buildShareURL(app) {
-      if (app === 'bejometer') {
-        let name1 = this.sanitize(this.input.bejometer.name1 || '')
-        let date1 = this.input.bejometer.date1
-        let name2 = this.sanitize(this.input.bejometer.name2 || '')
-        let date2 = this.input.bejometer.date2
-        return `${location.origin}/bejometer/${name1}:${date1}&${name2}:${date2}`
-      }
+    buildShareURLBejometer() {
+      let name1 = this.sanitize(this.input.bejometer.name1 || '')
+      let date1 = this.input.bejometer.date1 || ''
+      let name2 = this.sanitize(this.input.bejometer.name2 || '')
+      let date2 = this.input.bejometer.date2 || ''
+      return `${location.origin}/bejometer/${name1}:${date1}&${name2}:${date2}`
     },
 
     sanitize(name) {
