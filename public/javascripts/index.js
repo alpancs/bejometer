3a8a98d00198486b5e9d93b2026e42cbe365347a
Alfan Nur Fauzan <alpancs@gmail.com>
2017-03-25 17:08:24 +0700

    implement consultation and tebakgender

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,49 +11,115 @@ new Vue({
   el: '#app',
 
   data: {
-    input:
-      localStorage.input ?
-      JSON.parse(localStorage.input) :
-      {bejometer: {}, consultation: {}, tebakgender: {}},
+    bejometerInput: localStorage.bejometerInput ?
+      JSON.parse(localStorage.bejometerInput) : {},
+    consultationInput: localStorage.consultationInput ?
+      JSON.parse(localStorage.consultationInput) : {},
+    tebakgenderInput: localStorage.tebakgenderInput ?
+      JSON.parse(localStorage.tebakgenderInput) : {},
 
-    requestingBejometer: false,
-    resultBejometer: false,
-    errorBejometer: false,
-    shareURLBejometer: false,
+    bejometerRequesting: false,
+    bejometerResult: null,
+    bejometerError: null,
+    bejometerShareURL: null,
+
+    consultationRequesting: false,
+    consultationResult: null,
+    consultationError: null,
+    consultationShareURL: null,
+
+    tebakgenderRequesting: false,
+    tebakgenderResult: null,
+    tebakgenderError: null,
+    tebakgenderShareURL: null,
   },
 
   methods: {
-    submitBejometer() {
-      this.requestingBejometer = true
+    bejometerSubmit() {
+      this.bejometerRequesting = true
       this.openDialog('bejometerResult')
 
-      this.resultBejometer = null
-      this.shareURLBejometer = null
-      this.errorBejometer = null
+      this.bejometerResult = null
+      this.bejometerShareURL = null
+      this.bejometerError = null
 
       let params = {
-        name1: this.input.bejometer.name1,
-        date1: this.input.bejometer.date1,
-        name2: this.input.bejometer.name2,
-        date2: this.input.bejometer.date2,
+        name1: this.bejometerInput.name1,
+        date1: this.bejometerInput.date1,
+        name2: this.bejometerInput.name2,
+        date2: this.bejometerInput.date2,
       }
       axios.get('/api/bejometer', {params})
         .then((response) => {
-          this.resultBejometer = response.data
-          this.shareURLBejometer = this.buildShareURLBejometer()
+          this.bejometerResult = response.data
+          this.bejometerShareURL = this.bejometerBuildShareURL()
         })
-        .catch((error) => this.errorBejometer = error)
-        .then(() => this.requestingBejometer = false)
+        .catch((error) => this.bejometerError = error)
+        .then(() => this.bejometerRequesting = false)
 
-      localStorage.input = JSON.stringify(this.input)
+      localStorage.bejometerInput = JSON.stringify(this.bejometerInput)
     },
 
-    buildShareURLBejometer() {
-      let name1 = this.sanitize(this.input.bejometer.name1 || '')
-      let date1 = this.input.bejometer.date1 || ''
-      let name2 = this.sanitize(this.input.bejometer.name2 || '')
-      let date2 = this.input.bejometer.date2 || ''
+    consultationSubmit() {
+      this.consultationRequesting = true
+      this.openDialog('consultationResult')
+
+      this.consultationResult = null
+      this.consultationShareURL = null
+      this.consultationError = null
+
+      let params = {
+        name: this.consultationInput.name,
+        date: this.consultationInput.date,
+      }
+      axios.get('/api/consultation', {params})
+        .then((response) => {
+          this.consultationResult = response.data
+          this.consultationShareURL = this.consultationBuildShareURL()
+        })
+        .catch((error) => this.consultationError = error)
+        .then(() => this.consultationRequesting = false)
+
+      localStorage.consultationInput = JSON.stringify(this.consultationInput)
+    },
+
+    tebakgenderSubmit() {
+      this.tebakgenderRequesting = true
+      this.openDialog('tebakgenderResult')
+
+      this.tebakgenderResult = null
+      this.tebakgenderShareURL = null
+      this.tebakgenderError = null
+
+      let params = {name: this.tebakgenderInput.name}
+      axios.get('/api/tebakgender', {params})
+        .then((response) => {
+          this.tebakgenderResult = response.data
+          this.tebakgenderShareURL = this.tebakgenderBuildShareURL()
+        })
+        .catch((error) => this.tebakgenderError = error)
+        .then(() => this.tebakgenderRequesting = false)
+
+      localStorage.tebakgenderInput = JSON.stringify(this.tebakgenderInput)
+    },
+
+    bejometerBuildShareURL() {
+      let name1 = this.sanitize(this.bejometerInput.name1 || '')
+      let date1 = this.bejometerInput.date1 || ''
+      let name2 = this.sanitize(this.bejometerInput.name2 || '')
+      let date2 = this.bejometerInput.date2 || ''
       return `${location.origin}/bejometer/${name1}:${date1}&${name2}:${date2}`
+    },
+
+    consultationBuildShareURL() {
+      let name = this.sanitize(this.consultationInput.name || '')
+      let date = this.consultationInput.date || ''
+      return `${location.origin}/consultation/${name}:${date}`
+    },
+
+    tebakgenderBuildShareURL() {
+      let name = this.sanitize(this.tebakgenderInput.name || '')
+      return `${location.origin}/tebakgender/${name}`
     },
 
     sanitize(name) {
