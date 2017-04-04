a6f6878ad6770bdb81bdd38544b4a82143e97380
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-04 11:34:27 +0700

    static page bejometer result

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = new express.Router()
-// const bejometer = require('modules/bejometer')
-// const sanitize = require('tebakgender/lib/sanitize')
+const bejometer = require('modules/bejometer')
+const sanitize = require('tebakgender/lib/sanitize')
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   let data = {
     production: process.env.NODE_ENV === 'production',
   }
   res.render('index', data)
+})
+
+router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
+  let name1 = sanitize(req.params.name1)
+  let date1 = Date.parse(req.params.date1)
+  let name2 = sanitize(req.params.name2)
+  let date2 = Date.parse(req.params.date2)
+  res.json(bejometer(name1, date1, name2, date2))
 })
 
 module.exports = router
