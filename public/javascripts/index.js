548c17f4e690ec905e700f3914a0fc37c455d0bf
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-04 11:29:11 +0700

    bejometer share URL bug fix

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -98,11 +98,11 @@ new Vue({
 
     bejometerBuildShareURL(name1, date1, name2, date2) {
       name1 = this.sanitize(name1 || '').toLowerCase()
-      date1 = date1 || ''
+      date1 = date1 || new Date().toISOString().slice(0, 10)
       name2 = this.sanitize(name2 || '').toLowerCase()
-      date2 = date2 || ''
-      date1 = typeof a === 'number' ? new Date(date1).toISOString().slice(0, 10) : date1
-      date2 = typeof a === 'number' ? new Date(date2).toISOString().slice(0, 10) : date2
+      date2 = date2 || new Date().toISOString().slice(0, 10)
+      date1 = typeof date1 === 'number' ? new Date(date1).toISOString().slice(0, 10) : date1
+      date2 = typeof date2 === 'number' ? new Date(date2).toISOString().slice(0, 10) : date2
       return `${location.origin}/bejometer/${name1}:${date1}&${name2}:${date2}`
     },
 
