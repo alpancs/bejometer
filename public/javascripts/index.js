3c01ab773a141ff86c73038312afc9df84a51568
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-04 14:56:18 +0700

    500ms -> 1000ms

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -43,7 +43,7 @@ new Vue({
       axios.get('/api/bejometer', {params})
       .then((response) => {
         this.bejometerResult = response.data
-        return new Promise((resolve) => setTimeout(resolve, 500))
+        return new Promise((resolve) => setTimeout(resolve, 1000))
       })
       .then(() => {
         let genderConfidence1 = this.bejometerResult.person1.genderConfidence
@@ -71,7 +71,7 @@ new Vue({
       axios.get('/api/consultation', {params})
       .then((response) => {
         this.consultationResult = response.data
-        return new Promise((resolve) => setTimeout(resolve, 500))
+        return new Promise((resolve) => setTimeout(resolve, 1000))
       })
       .then(() => {
         if (this.human(this.consultationResult.person.genderConfidence))
@@ -92,7 +92,7 @@ new Vue({
       axios.get('/api/tebakgender', {params})
       .then((response) => {
         this.tebakgenderResult = response.data
-        return new Promise((resolve) => setTimeout(resolve, 500))
+        return new Promise((resolve) => setTimeout(resolve, 1000))
       })
       .then(() => this.openDialog('tebakgenderResult'))
       .catch(() => this.$refs.error.open())
