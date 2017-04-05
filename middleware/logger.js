ba1c2678efbc7807650288759f3384509543f527
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-05 14:51:36 +0700

    remove debug

--- middleware/logger.js
+++ middleware/logger.js
@@ -6,7 +6,6 @@ if (process.env.NODE_ENV === 'production') {
   let option = {
     db: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`,
   }
-  console.log(option);
   winston.add(winston.transports.MongoDB, option)
 }
 
