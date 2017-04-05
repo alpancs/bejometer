0053edd2e63e5275198659a6ea9811727e47b23e
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-05 19:09:22 +0700

    dummy static page

---
+++ public/javascripts/bejometer.js
@@ -0,0 +1,12 @@
+Vue.use(VueMaterial)
+
+Vue.material.registerTheme('default', {
+  primary: 'pink',
+  accent: 'purple',
+  warn: 'deep-orange',
+  background: 'white',
+})
+
+new Vue({
+  el: '#app',
+})
--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,17 @@ router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
   let date1 = Date.parse(req.params.date1)
   let name2 = sanitize(req.params.name2)
   let date2 = Date.parse(req.params.date2)
-  res.json(bejometer(name1, date1, name2, date2))
+  let result = bejometer(name1, date1, name2, date2)
+  let data = {
+    production: process.env.NODE_ENV === 'production',
+    name1,
+    date1,
+    name2,
+    date2,
+    result,
+    description: `Hasil Bejometer ${name1} dengan ${name2}`,
+  }
+  res.render('bejometer', data)
 })
 
 module.exports = router
