bb2b917a935845d8dd5fdea76076184deaf69fde
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-06 16:20:43 +0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const bejometer = require('modules/bejometer')
 const sanitize = require('tebakgender/lib/sanitize')
+const title = {L: 'mas', P: 'mbak'}
 
 router.get('/', (req, res) => {
   let data = {
@@ -11,15 +12,17 @@ router.get('/', (req, res) => {
 })
 
 router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
-  let name1 = toTitleCase(sanitize(req.params.name1))
+  let name1 = req.params.name1
   let date1 = Date.parse(req.params.date1)
-  let name2 = toTitleCase(sanitize(req.params.name2))
+  let name2 = req.params.name2
   let date2 = Date.parse(req.params.date2)
-
   let result = bejometer(name1, date1, name2, date2)
-  let percent = Math.round(result.match * 10000) / 100 + '%'
+
+  name1 = toTitleCase(title[result.person1.estimation.gender] + ' ' + sanitize(name1))
+  name2 = toTitleCase(title[result.person2.estimation.gender] + ' ' + sanitize(name2))
   let day1 = toTitleCase(result.person1.indonesiaDay + ' ' + result.person1.javaDay)
   let day2 = toTitleCase(result.person2.indonesiaDay + ' ' + result.person2.javaDay)
+  let percent = Math.round(result.match * 10000) / 100 + '%'
 
   let data = {
     production: process.env.NODE_ENV === 'production',
