13f1e85bab1bd83d16efe75ff4f017a13a3452d8
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-06 11:38:30 +0700

    refactor

--- routes/index.js
+++ routes/index.js
@@ -15,20 +15,16 @@ router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
   let date1 = Date.parse(req.params.date1)
   let name2 = toTitleCase(sanitize(req.params.name2))
   let date2 = Date.parse(req.params.date2)
+
   let result = bejometer(name1, date1, name2, date2)
   let percent = Math.round(result.match * 10000) / 100 + '%'
   let day1 = toTitleCase(result.person1.indonesiaDay + ' ' + result.person1.javaDay)
   let day2 = toTitleCase(result.person2.indonesiaDay + ' ' + result.person2.javaDay)
+
   let data = {
     production: process.env.NODE_ENV === 'production',
-    name1,
-    date1,
-    day1,
-    name2,
-    date2,
-    day2,
-    result,
     description: `Hasil Bejometer ${name1} dengan ${name2}: ${percent} jodoh!`,
+    name1, date1, day1, name2, date2, day2, result,
   }
   res.render('bejometer', data)
 })
