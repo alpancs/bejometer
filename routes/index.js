7eb31d1e500e7c024d71f99673640526796cd345
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-06 10:40:13 +0700

    refine description

--- routes/index.js
+++ routes/index.js
@@ -11,21 +11,28 @@ router.get('/', (req, res) => {
 })
 
 router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
-  let name1 = sanitize(req.params.name1)
+  let name1 = toTitleCase(sanitize(req.params.name1))
   let date1 = Date.parse(req.params.date1)
-  let name2 = sanitize(req.params.name2)
+  let name2 = toTitleCase(sanitize(req.params.name2))
   let date2 = Date.parse(req.params.date2)
   let result = bejometer(name1, date1, name2, date2)
+  let percent = Math.round(result.match * 10000) / 100 + '%'
+  let day1 = toTitleCase(result.person1.indonesiaDay + ' ' + result.person1.javaDay)
+  let day2 = toTitleCase(result.person2.indonesiaDay + ' ' + result.person2.javaDay)
   let data = {
     production: process.env.NODE_ENV === 'production',
     name1,
     date1,
+    day1,
     name2,
     date2,
+    day2,
     result,
-    description: `Hasil Bejometer ${name1} dengan ${name2}`,
+    description: `Hasil Bejometer ${name1} (${day1}) dengan ${name2} (${day2}): ${percent} jodoh!`,
   }
   res.render('bejometer', data)
 })
+
+let toTitleCase = (text) => text.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase()+word.slice(1)).join(' ')
 
 module.exports = router
