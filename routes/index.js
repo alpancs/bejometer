df2067de98f6a685d5e087e21cae2368fb0bcae7
Alfan Nur Fauzan <alpancs@gmail.com>
2017-04-08 10:31:46 +0700

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -12,22 +12,22 @@ router.get('/', (req, res) => {
 })
 
 router.get('/bejometer/:name1::date1&:name2::date2', (req, res) => {
-  let name1 = req.params.name1
+  let name1 = sanitize(req.params.name1)
   let date1 = Date.parse(req.params.date1)
-  let name2 = req.params.name2
+  let name2 = sanitize(req.params.name2)
   let date2 = Date.parse(req.params.date2)
-  let result = bejometer(name1, date1, name2, date2)
 
-  name1 = toTitleCase(title[result.person1.estimation.gender] + ' ' + sanitize(name1))
-  name2 = toTitleCase(title[result.person2.estimation.gender] + ' ' + sanitize(name2))
+  let result = bejometer(name1, date1, name2, date2)
+  let title1 = title[result.person1.estimation.gender]
+  let title2 = title[result.person2.estimation.gender]
   let day1 = toTitleCase(result.person1.indonesiaDay + ' ' + result.person1.javaDay)
   let day2 = toTitleCase(result.person2.indonesiaDay + ' ' + result.person2.javaDay)
   let percent = Math.round(result.match * 10000) / 100 + '%'
 
   let data = {
     production: process.env.NODE_ENV === 'production',
-    description: `Hasil Bejometer ${name1} (${day1}) dengan ${name2} (${day2})`,
-    name1, date1, day1, name2, date2, day2, percent, result,
+    description: `Hasil Bejometer ${title1} ${name1} (${day1}) dengan ${title2} ${name2} (${day2})`,
+    title1, name1, day1, title2, name2, day2, percent,
   }
   res.render('bejometer', data)
 })
